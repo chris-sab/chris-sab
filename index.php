19b217293314abb0ff9beb803e2942a0e79a0363
Christopher Saburo <christopher.saburo@outlook.com>
2020-10-28 16:59:18 +0900

    update

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
           Christopher Saburo
         </h1>
         <h2 class="desc" style="color: white; font-size: 20px;">
-          Senior VueJS | NuxtJS | Quasar | Gridsome | React | NextJS | Gatsby | NodeJS Developer
+          Senior VueJS | SvelteJS | NuxtJS | Quasar | Gridsome | ElderJS | Sapper Developer
         </h2>
         <ul class="social list-inline">
           <li class="list-inline-item"><a href="https://www.linkedin.com/in/christopher-saburo-048a771a2/"><i
@@ -47,38 +47,24 @@
             <h2 class="heading">About Me</h2>
             <p style="font-size: 1.2rem;">
               Experienced senior web developer with a demonstrated history of working in
-              marketing, education. Very top-rated skilful in vuejs, nuxtjs, quasar, react,
-              gridsome, nodejs and proficient in firebase, aws cloud service.
+              marketing, education. Very top-rated skilful in vuejs, sveltejs, nuxtjs, quasar, sepper,
+              gridsome.
             </p>
             <div class="content">
               <p>
-                - <strong>Vue Frontend Development</strong><br>
-                VueJS, Vue 2, Vue-CLI, Nuxt.js, Quasar, Gridsome, VueXORM, VueXSAX, TailwindCSS, Vuetify
-              </p>
-              <p>
-                - <strong>React Frontend Development</strong><br>
-                React, NextJS, Gatsby, Redux, Saga/Thunk, React-Material-UI, React-Bootstrap, React-Apollo, GraphQL, React Native
+                - <strong>Frontend Development</strong><br>
+                VueJS, Vue 2, Vue 3, Vue-CLI, SvelteJS, NuxtJS, ElderJS, Quasar, Gridsome, Sapper, VueXORM, VueSaX, TailwindCSS, Vuetify
               </p>
               <p>
                 - <strong>Backend RESTful API Development/Integration</strong><br>
                 Node.js, Nest.js, Express.js, PHP / Laravel / CI
-              </p>
-              <p>
-                - <strong>Firebase Development/Deployment</strong><br>
-                Firebase Authentication, Firebase Realtime Database, Firebase Could Store, Firebase
-                Cloud functions, Firebase Analytics,
-                Firebase Hosting, Firebase-CLI
-              </p>
-              <p>
-                - <strong>Amazon Web Service Development/Deployment</strong><br>
-                AWS Amplify, AWS Lambda, Amazon Cognito, Amazon EC2, Amazon S3, AWS CLI
               </p>
             </div>
             <hr>
             <div style="display: flex;">
               <div style="margin-right: 5rem;">
                 <div style="font-size: 1.3rem; font-weight: 400; color: green; margin-top: 1rem;">
-                  $30.00</div>
+                  $25</div>
                 <div>Hourly rate</div>
               </div>
               <div>
@@ -204,6 +190,31 @@
               </div>
 
               <div class="item row">
+                <a class="col-md-5 col-12" href="https://www.iota.org/" target="_blank">
+                  <img class="img-fluid project-image" src="assets/images/projects/project-15.png" alt="project name" />
+                </a>
+                <div class="col-md-7 col-12">
+                  <h3 class="title">
+                    <a href="https://www.iota.org/" target="_blank">
+                      IOTA Empowering Data Management CMS Site
+                    </a>
+                  </h3>
+                  <p class="mb-2">
+                    - <strong>Develop Stack</strong> :<br />
+                    SvelteJS / Sapper / Contentful CMS / NodeJS / API Integration
+                  </p>
+                  <p class="mb-2">
+                    - <strong>What I have done</strong> :<br />
+                    <li><strong>Scratch Development</strong></li>
+                    <li>Made FrontEnd side with SvelteJS/Sapper</li>
+                    <li>Made UI Components, Pages, Layout Strucutre From Scratch </li>
+                    <li>Integrated Contentful CMS</li>
+                    <li>Integrated NodeJS Backend</li>
+                  </p>
+                </div>
+              </div>
+
+              <div class="item row">
                 <a class="col-md-5 col-12" href="https://www.learna.ac.uk//" target="_blank">
                   <img class="img-fluid project-image" src="assets/images/projects/project-10.png" alt="project name" />
                 </a>
@@ -227,6 +238,31 @@
                     <li>Integrated 3rd Party API</li>
                     <li>Made it Mobile Responsive</li>
                     <li>Used Bootstrap-Vue</li>
+                  </p>
+                </div>
+              </div>
+
+              <div class="item row">
+                <a class="col-md-5 col-12" href="https://electrofrenorr.co/" target="_blank">
+                  <img class="img-fluid project-image" src="assets/images/projects/project-16.png" alt="project name" />
+                </a>
+                <div class="col-md-7 col-12">
+                  <h3 class="title">
+                    <a href="https://electrofrenorr.co/" target="_blank">
+                      Electron Frenorr Company Site
+                    </a>
+                  </h3>
+                  <p class="mb-2">
+                    - <strong>Develop Stack</strong> :<br />
+                    SvelteJS / Bootstrap / API Integration
+                  </p>
+                  <p class="mb-2">
+                    - <strong>What I have done</strong> :<br />
+                    <li><strong>Scratch Development</strong></li>
+                    <li>Made FrontEnd side with SvelteJS</li>
+                    <li>Made UI Components, Pages, Layout Strucutre From Scratch </li>
+                    <li>Used Bootstrap</li>
+                    <li>Integrated API with Backend</li>
                   </p>
                 </div>
               </div>
@@ -259,6 +295,29 @@
                 </div>
               </div>
 
+              <div class="item row">
+                <a class="col-md-5 col-12" href="https://whitetriangle.agency/" target="_blank">
+                  <img class="img-fluid project-image" src="assets/images/projects/project-17.png" alt="project name" />
+                </a>
+                <div class="col-md-7 col-12">
+                  <h3 class="title">
+                    <a href="https://whitetriangle.agency/" target="_blank">
+                      White-Triangle Agency Site
+                    </a>
+                  </h3>
+                  <p class="mb-2">
+                    - <strong>Develop Stack</strong> :<br />
+                    SvelteJS / SquareSpace CMS / Tailwind CSS
+                  </p>
+                  <p class="mb-2">
+                    - <strong>What I have done</strong> :<br />
+                    <li><strong>Scratch Development</strong></li>
+                    <li>Made FrontEnd side with SvelteJS</li>
+                    <li>Integrated SquareSpace CMS with Frontend</li>
+                    <li>Used Tailwind CSS</li>
+                  </p>
+                </div>
+              </div>
 
               <div class="item row">
                 <a class="col-md-5 col-12" href="https://www.realtimereviews.com/" target="_blank">
